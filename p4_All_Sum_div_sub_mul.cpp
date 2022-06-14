586f0cffa3224292ef5bda853a9997fb99a3b733
Adesh Khole <adeshkhole8686@gmail.com>
2022-06-14 22:47:52 +0530

    all operator sum,mul,div,sub

---
+++ p4_All_Sum_div_sub_mul.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter The Number :";
+    cin>>a>>b;
+    cout<<"Addition of number :"<<a+b<<endl;
+    cout<<"Substraction of number :"<<a-b<<endl;
+    cout<<"Multiplication of number :"<<a*b<<endl;
+    cout<<"Division of number :"<<a/b<<endl;
+    
+    return 0;
+}
